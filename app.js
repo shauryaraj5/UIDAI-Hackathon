2ceace7a3c303e1ae038b6acad4169de4ed6a8f0
misima49 <56494010+misima49@users.noreply.github.com>
2021-10-31 17:46:26 +0530

    Modify casing and output fields

--- app.js
+++ app.js
@@ -8,56 +8,64 @@ app.use(express.json());
 
 app.post("/", function (req, res) {
 
-    let originalAdd = req.body;
+    //Change all key values to lower case using lowercaseKyes()
 
+    const lowercaseKeys = obj =>
+        Object.keys(obj).reduce((acc, key) => {
+            acc[key.toLowerCase()] = obj[key];
+            return acc;
+        }, {});
+
+    let originalAdd = lowercaseKeys(req.body);
+
+    //Change sequence of keys according to UIDAI address format
     let addSkeleton = {
-        "House": null,
-        "Building": null,
-        "Apartment": null,
-        "Street": null,
-        "Road": null,
-        "Lane": null,
-        "Area": null,
-        "Locality": null,
-        "Sector": null,
-        "Landmark": null,
-        "Village": null,
-        "Town": null,
-        "City": null,
-        "Sub District": null,
-        "District": null,
-        "State": null,
-        "Pincode": null
+        "house": null,
+        "building": null,
+        "apartment": null,
+        "street": null,
+        "road": null,
+        "lane": null,
+        "area": null,
+        "locality": null,
+        "sector": null,
+        "landmark": null,
+        "village": null,
+        "town": null,
+        "city": null,
+        "sub district": null,
+        "district": null,
+        "state": null,
+        "pincode": null
     }
 
     let modAdd = {
-        "Pincode": null,
-        "State": null,
-        "District": null,
-        "Sub District": null,
-        "City": null,
-        "Town": null,
-        "Village": null,
-        "Landmark": null,
-        "Sector": null,
-        "Locality": null,
-        "Area": null,
-        "Lane": null,
-        "Road": null,
-        "Street": null,
-        "Apartment": null,
-        "Building": null,
-        "House": null
+        "pincode": null,
+        "state": null,
+        "district": null,
+        "sub sistrict": null,
+        "city": null,
+        "town": null,
+        "village": null,
+        "landmark": null,
+        "sector": null,
+        "locality": null,
+        "area": null,
+        "lane": null,
+        "road": null,
+        "street": null,
+        "apartment": null,
+        "building": null,
+        "house": null
     }
 
+    //Handles casing of resultant address
     const formatString = (str) => {
         const specialCharRegex = new RegExp("\\W|_");
-        while(specialCharRegex.test(str[0]))
-        {
+        while (specialCharRegex.test(str[0])) {
             str = str.replace(str[0], "");
         }
-        while(specialCharRegex.test(str[str.length - 1]))
-        {
+        while (specialCharRegex.test(str[str.length - 1])) {
             str = str.replace(str[str.length - 1], "");
         }
 
@@ -70,10 +78,10 @@ app.post("/", function (req, res) {
 
     let addSt = new Set();
 
+    //Remove duplication and separators in address
     for (const field in originalAdd) {
         if (originalAdd[field] !== null) {
             let modAdd = originalAdd[field].split((/(?:, | ,|,|\r|\n)+/));
-            // console.log(modAdd);
 
             modAdd.forEach(element => {
                 if (addSt.has(element.toLowerCase())) {
@@ -85,50 +93,42 @@ app.post("/", function (req, res) {
         }
     }
 
-    // console.log(originalAdd);
     originalAdd = Object.assign(addSkeleton, originalAdd);
 
-    for(let key in originalAdd) {
-        if(originalAdd[key] !== null)
+    for (let key in originalAdd) {
+        if (originalAdd[key] !== null)
             originalAdd[key] = formatString(originalAdd[key]);
     }
 
-    // console.log(originalAdd);
-
-    let formattedAddress = `${originalAdd.House ? originalAdd.House : ""}${originalAdd.Building ? ", "+originalAdd.Building : ""}, ${originalAdd.Apartment ? ", "+originalAdd.Apartment+"," : ""}
-    ${originalAdd.Street ? originalAdd.Street : ""}${originalAdd.Road ? ", "+originalAdd.Road : ""}${originalAdd.Lane ? ", "+originalAdd.Lane+"," : ""}
-    ${originalAdd.Area ? originalAdd.Area : ""}${originalAdd.Locality ? ", "+originalAdd.Locality : ""}${originalAdd.Sector ? ", "+originalAdd.Sector+"," : ""}
-    ${originalAdd.Landmark ? +originalAdd.Landmark+"," : ""}
-    ${originalAdd.Vilage ? originalAdd.Village : ""}${originalAdd.Town ? ", "+originalAdd.Town : ""}${originalAdd.City ? ", "+originalAdd.City+"," : ""}
-    ${originalAdd["Sub District"] ? originalAdd["Sub District"] : ""}${originalAdd.District ? ", "+originalAdd.District+"," : ""}
-    ${originalAdd.State ? originalAdd.State : ""}${originalAdd.Pincode ? " - "+originalAdd.Pincode : "."}`;
+    //Make the final complete address obtained from above modifications
+    let formattedAddress = `${originalAdd.house ? originalAdd.house : ""}${originalAdd.building ? "," + originalAdd.building : ""}, ${originalAdd.apartment ? "," + originalAdd.apartment + "," : ""}
+    ${originalAdd.street ? originalAdd.street : ""}${originalAdd.road ? "," + originalAdd.road : ""}${originalAdd.lane ? "," + originalAdd.lane + "," : ""}
+    ${originalAdd.area ? originalAdd.area : ""}${originalAdd.locality ? "," + originalAdd.locality : ""}${originalAdd.sector ? "," + originalAdd.sector + "," : ""}
+    ${originalAdd.landmark ? originalAdd.landmark + ", " : ""}
+    ${originalAdd.village ? originalAdd.village : ""}${originalAdd.town ? ", " + originalAdd.town : ""}${originalAdd.city ? ", " + originalAdd.city + "," : ""}
+    ${originalAdd["Sub district"] ? originalAdd["Sub district"] : ""}${originalAdd.district ? ", " + originalAdd.district + ", " : ""}
+    ${originalAdd.state ? originalAdd.state : ""}${originalAdd.pincode ? " - " + originalAdd.pincode : "."}`;
 
     formattedAddress = formattedAddress.split("\n");
     let finalAddress = [];
     let re = new RegExp("\\W|_");
 
     formattedAddress.forEach((str) => {
-        if(str.trim() !== "")
+        if (str.trim() !== "")
             finalAddress.push(str);
     });
 
     finalAddress.forEach((str, i) => {
-        while(re.test(str[0]))
-        {
+        while (re.test(str[0])) {
             str = str.replace(str[0], "");
         }
         finalAddress[i] = str;
     })
 
-    finalAddress = finalAddress.join("\n");
+    finalAddress = finalAddress.join("");
     originalAdd.formatted_address = finalAddress;
 
-    // for (const removeNull in originalAdd) {
-    //     if (originalAdd[removeNull] === null) {
-    //         delete originalAdd[removeNull];
-    //     }
-    // }
-
+    //Send the formatted address object
     res.send(originalAdd);
 
 });
